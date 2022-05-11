9ace1d39b599e6490eb65041feb7c437b363396f
jethanstory <jstory@csu.fullerton.edu>
2022-05-10 20:16:32 -0700

    Added speed variable to control speed of camera

--- Test Project 2D/Assets/Scripts/CameraMovement.cs
+++ Test Project 2D/Assets/Scripts/CameraMovement.cs
@@ -7,6 +7,8 @@ public class CameraMovement : MonoBehaviour
     public GameObject PlayerGameObject;
     //public Player player;
     public Camera camera1;
+
+    public int speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +17,10 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float step = 1;
+        float step = 1; //2 is hard mode
 
         var cameraPosition = Camera.main.gameObject.transform.position;
-        cameraPosition.y += -step / 50;
+        cameraPosition.y += -step / speed; //50
         Camera.main.gameObject.transform.position = cameraPosition;
 
         //if (camera1.transform.position.y > PlayerGameObject.transform.position.y + 100)
